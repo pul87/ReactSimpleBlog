158bb6c0d1d136752e74a54195c2214a126f1d65
Paolo Basso <paolo.basso87@gmail.com>
2017-01-24 17:33:37 +0100

    Aggiunta creazione, delete, show lista, author

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -10,13 +10,36 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post)=> {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={"posts/" + post.id}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <div className="text-xs-right">
-        <Link to="posts/new" className="btn btn-primary">Add Post</Link>
+      <div>
+        <div className="text-xs-right">
+          <Link to="posts/new" className="btn btn-primary">Add Post</Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
       </div>
     );
   }
 };
 
-export default connect(null, { fetchPosts } )(PostsIndex);
+function mapStateToProps(state) {
+  return {
+    posts: state.posts.all
+  };
+}
+
+export default connect(mapStateToProps, { fetchPosts } )(PostsIndex);
